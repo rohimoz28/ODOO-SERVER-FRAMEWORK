0dd5103137b4e894cd614a2e0b5511225cd68abb
Rohim Muhamad <rohimuhamadd@gmail.com>
2026-04-16 10:18:35 +0700

    Chapter 2 : Adding New Module

---
+++ customize/estate_property/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate_property",
+
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+
+    'description': """
+Real Estate Property Module
+    """,
+
+    'author': "Rohim Muhamad",
+    'website': "https://rohimoz28.netlify.app",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    # Gunakan ini supaya modul bisa ditampilkan dengan filter 'Apps'
+    'installable': True,
+    'application': True,
+}
